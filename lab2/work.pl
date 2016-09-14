47a7f04861cf9684b6a06ec9ae149ae7874d81f5
robben1234 <c.gavrilenko.96@gmail.com>
2016-09-14 10:40:31 +0300

    fix(lab2): syntax fix

--- lab2/work.pl
+++ lab2/work.pl
@@ -1,26 +1,35 @@
 predicates:
+% parent, sibling
   parent(symbol, symbol).
   man(symbol).
   woman(symbol).
+  % sibling, parent
   mother(A, B):-
     parent(B, A), woman(B).
+  % sibling, parent
   father(A, B):-
     parent(B, A), man(B).
+  % sibling, parent
   son(A, B):-
     parent(B, A), man(A).
+  % sibling, parent
   daughter(A, B):-
     parent(B, A), woman(A).
+  % sibling, parent
   grandson(A, B):-
     parent(B, C), son(A, C).
+  % sibling, parent
   granddaughter(A, B):-
     parent(B, C), daughter(A, C).
+  % parent, sibling
   ancestor(A, B):-
     parent(A, B).
+  % parent, sibling
   ancestor(A, B):-
     parent(A, C), ancestor(C, B).
-clauses:
-  parent(tamara,oleg).
+% clauses:
   parent(leonid,oleg).
+  parent(tamara,oleg).
   parent(vera, irina).
   parent(timur, irina).
   parent(oleg, kirill).
